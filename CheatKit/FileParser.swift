69b2f8e1c46c0005df9949b81db8d43a4f104983
Rpwnage <orksec@protonmail.com>
2021-04-22 09:36:47 +0200

    initial commit

---
+++ CheatKit/FileParser.swift
@@ -0,0 +1,18 @@
+//
+//  FileParser.swift
+//  CheatKit
+//
+//  Created by Luca on 22.04.21.
+//
+
+import Foundation
+
+public class FileParser {
+    public init() {
+        // Init function
+    }
+    
+    public func readPlist(filepath: String) {
+        /* Read the contents of a plist file */
+    }
+}
--- CheatKit/fileTypes/fileParser.swift
+++
@@ -1,8 +0,0 @@
-//
-//  plist.swift
-//  CheatKit
-//
-//  Created by Luca on 22.04.21.
-//
-
-import Foundation
