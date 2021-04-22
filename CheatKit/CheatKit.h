6fab19262dc5c669a2f0dc1826347ccfdebe3b11
Rpwnage <orksec@protonmail.com>
2021-04-22 09:29:11 +0200

    Initial Commit

---
+++ CheatKit/CheatKit.h
@@ -0,0 +1,18 @@
+//
+//  CheatKit.h
+//  CheatKit
+//
+//  Created by Luca on 22.04.21.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CheatKit.
+FOUNDATION_EXPORT double CheatKitVersionNumber;
+
+//! Project version string for CheatKit.
+FOUNDATION_EXPORT const unsigned char CheatKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CheatKit/PublicHeader.h>
+
+
