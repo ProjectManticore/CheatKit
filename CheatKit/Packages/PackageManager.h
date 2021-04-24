3565faea7eee752f26d2417a1104140716e618e9
Rpwnage <orksec@protonmail.com>
2021-04-24 18:18:41 +0200

    Fixed gitignore

---
+++ CheatKit/Packages/PackageManager.h
@@ -0,0 +1,18 @@
+//
+//  PackageManager.h
+//  CheatKit Framework
+//
+//  Created by Luca on 24.04.21.
+//
+
+#ifndef PackageManager_h
+#define PackageManager_h
+
+
+@interface PackageManager : NSObject
+
+- (BOOL)isCheatInstalled:(NSString *)cheatID;
+
+@end
+
+#endif /* PackageManager_h */
---
+++ CheatKit/Packages/PackageManager.m
@@ -0,0 +1,19 @@
+//
+//  PackageManager.m
+//  CheatKit Framework
+//
+//  Created by Luca on 24.04.21.
+//
+
+#import <Foundation/Foundation.h>
+#include "PackageManager.h"
+
+@implementation PackageManager
+
+- (BOOL)isCheatInstalled:(NSString *)cheatID {
+    return NO;
+}
+
+@end
+
+
